168a15f68869bd67ab6bc491014c5f888961de1c
rebaz36 <66506429+rebaz36@users.noreply.github.com>
2021-04-27 12:44:01 +0300

    Bug Fix (#13)
    close #12

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Start Coding
+          Start Coding Test
         </p>
         <a
           className="App-link"
