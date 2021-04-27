60305772896b4cec7b1c47c8d33d6173fcc94f82
rebaz36 <rebaz415@gmail.com>
2021-04-27 12:50:03 +0300

    creating navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Start Coding Test
+          Start Coding Test1
         </p>
         <a
           className="App-link"
