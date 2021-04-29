02c89b569dc11b9f27747e6329d7dbfbfba167c2
Sima-J <76180062+Sima-J@users.noreply.github.com>
2021-04-29 11:25:34 +0300

    Update i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./locales/en.json";
+import kr from "./locales/kr.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+
+  kr: {
+    translation: kr,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+export default i18n;
