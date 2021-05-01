dab4f4e82dd5ae95303cbdb356b76382d9739400
Sima-J <sima.jazaa@gmail.com>
2021-05-02 00:58:28 +0300

    BugFix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-fragments */
 import React, { Fragment } from 'react'
 import { Switch, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './containers/Home'
@@ -26,15 +27,18 @@ function App() {
   return (
     <Fragment>
       <Navbar />
-      <Switch>
-        <Route exact path={HOME_ROUTE} component={Home} />
-        <Route path={CONTACT_ROUTE} component={Contact} />
-        <Route path={ABOUT_ROUTE} component={About} />
-        <Route path={PROFILE_ROUTE} component={Profile} />
-        <Route path={ADD_DONATION_ROUTE} component={AddDonation} />
-        <Route path={DONATION_DETAILS_ROUTE} component={DonationDetails} />
-        <Route path={SEARCH_ROUTE} component={Search} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={HOME_ROUTE} component={Home} />
+          <Route path={CONTACT_ROUTE} component={Contact} />
+          <Route path={ABOUT_ROUTE} component={About} />
+          <Route path={PROFILE_ROUTE} component={Profile} />
+          <Route path={ADD_DONATION_ROUTE} component={AddDonation} />
+          <Route path={DONATION_DETAILS_ROUTE} component={DonationDetails} />
+          <Route path={SEARCH_ROUTE} component={Search} />
+        </Switch>
+      </BrowserRouter>
+
       <Footer />
     </Fragment>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -8,9 +7,7 @@ import './i18n'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
