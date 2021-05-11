0cd57d8ff5cc67f2c4fbf4022ff08a17b37c0075
Sana-JZ <76180135+Sana-JZ@users.noreply.github.com>
2021-05-11 18:58:51 +0300

    Create Contact Form Card (#31)

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import ContactForm from '../../components/ContactForm'
+ 
 export default function Contact() {
     return (
         <div>
-           Contact Page 
+        <ContactForm />
         </div>
     )
 }
