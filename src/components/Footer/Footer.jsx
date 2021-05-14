1104775aad9f2b923a6db47f281ac9df918f05a0
rebaz36 <rebaz415@gmail.com>
2021-05-14 16:26:01 +0300

    Footer Finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,138 @@
-import React from 'react'
+import React from "react";
+ import { Nav, Col,Row, Navbar, Image} from 'react-bootstrap'
+ import {NavLink} from 'react-router-dom'
+ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+ import logo from '../../assets/logo.svg'
+ // import call from '../../assets/call.png'
+ // import email from '../../assets/email.png'
+//  import fb from '../../assets/fb.png'
+//  import instagram from '../../assets/instagram.png'
+//  import twitter from '../../assets/twitter.png'
 
-export default function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
+import { CONTACT_ROUTE, HOME_ROUTE, ABOUT_ROUTE, PROFILE_ROUTE,SEARCH_ROUTE} from '../../router'
+import "./Footer.scss";
+  
+const Footer = () => {
+  return (
+    <footer className = "cont">
+       
+      <Row>
+
+          <Col className = "col" >
+
+          Menu <h6 className="draw-line1"> </h6>
+
+            <h7 className="list-unstyled">
+                <li>
+                    <Nav.Link className="lin" 
+                        exact as={NavLink} to={HOME_ROUTE}>Home
+                    </Nav.Link>
+                </li>
+          
+                <li>
+                    <Nav.Link  className="lin"
+                        as={NavLink} to={PROFILE_ROUTE}>Profile
+                    </Nav.Link>
+                </li>
+          
+                <li>
+                    <Nav.Link className="lin"  
+                        as={NavLink} to={CONTACT_ROUTE}>Contact
+                    </Nav.Link>
+                </li>
+
+                <li>
+                    <Nav.Link  className="lin"  
+                        as={NavLink} to={ABOUT_ROUTE}>About
+                    </Nav.Link>
+                </li>
+          
+            </h7>
+
+          </Col>
+
+          <Col>
+          Main Types <h6 className="draw-line2"> </h6>
+            <h7 className="list-unstyled">
+                 
+                 <li>
+                   <Nav.Link  className="lin"
+                     as={NavLink} to={SEARCH_ROUTE}>Donation
+                   </Nav.Link>
+                 </li>
+
+                 <li>Requested Donations</li>
+
+            </h7>
+
+          </Col>
+                        
+          <Col>
+
+          Contact Us <h6 className="draw-line3"> </h6>
+            <h7 className="list-unstyled">
+                <li>
+                    <span > 
+                        <a className = 'text' href={`tel:${"+234081-1236-4568"}`}>
+                        <FontAwesomeIcon className="icon" icon="phone"/>{'  '}
+                         +234081-1236-4568 
+                        </a> 
+                    </span>
+
+                    <br/>
+
+                    <span>
+                      <a className = 'text' href={`mailto:${"contact@KFD.com"}`}>
+                         <FontAwesomeIcon className="icon" icon="envelope"/>{'  '}
+                        Contact@KFD.com
+                      </a> 
+                    </span>
+                </li>
+
+            </h7>
+
+          </Col>
+
+          <Col>
+            Follow Us <h6 className="draw-line4" > </h6>
+            <h7 className = "list-unstyled">
+                <li className = "SocialProfile">
+                
+                  <a href="https://www.instagram.com/">
+                    <FontAwesomeIcon icon={['fab', 'instagram']} className="icon hover:text-darkgray fa-2x mx-2"/>
+                  </a>
+
+                  <a href="https://www.facebook.com/">
+                    <FontAwesomeIcon icon={['fab', 'facebook-square']} className="icon hover:text-darkgray fa-2x mx-2"/>
+                  </a>
+
+                  <a href="https://www.twitter.com/">
+                    <FontAwesomeIcon icon={['fab', 'twitter']} className="icon hover:text-darkgray fa-2x mx-2"/>
+                  </a>
+            
+                </li>
+            </h7>
+
+          </Col>
+
+      </Row>
+
+      <Row style={{borderTop: '3px solid red'}}>
+                 
+          <Col>
+
+            <p className="text"> 
+              <Navbar.Brand href="/" className = 'text'>
+
+                <Image src={logo} alt="logo"/>
+                © Copyright 2021 - KFD 
+
+              </Navbar.Brand> 
+
+            </p>
+
+          </Col>
+      </Row>
+
+    </footer>
+  ); }; export default Footer;
