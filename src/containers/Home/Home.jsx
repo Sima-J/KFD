7a9c27fafb4dd0b44af0dcb63c24aaf13da7e85f
jellywiz <muzaffer.dler@gmail.com>
2021-05-22 01:42:32 +0300

    Homehedder

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import './Home.scss'
+import {
+    Row,
+    Col,
+    Container,
+    Image } from 'react-bootstrap'
+import HomePic from '../../assets/HomePic.png'
 
 export default function Home() {
     return (
-        <div>
-            Home page KFD   
+        <div className = "back">
+            <Container>
+            <Row>
+                <Col>
+                <Image  
+                    src={HomePic}
+                fluid />
+                </Col>
+                <Col className = "text">
+                    <h4>The Food Donation WebApp is an online platform that works as a bridge to connect restaurants, food-supplier companies and  Supermarkets to organization. </h4>
+                </Col>
+            </Row>
+        </Container>
         </div>
+        
     )
 }
