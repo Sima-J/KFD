a40b777639a99b2401bdf3f6a15a71ab4f182cbb
Sima-J <76180062+Sima-J@users.noreply.github.com>
2021-05-25 21:45:50 +0300

    Update Profile.jsx

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image} from 'react-bootstrap'
 import ProfilePic from '../../assets/ProfilePic.png'
 import Cover from '../../assets/ProfileBackground.png'
-import ProfileCard from './../../components/ProfileCard';
+import ProfileCard from '../../components/ProfileCard';
 
 function Profile() {
     return(
