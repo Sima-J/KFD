12df328feba7aa437d78a17422b2e23a24c06693
Sima-J <76180062+Sima-J@users.noreply.github.com>
2021-05-25 21:42:51 +0300

    Update Profile.jsx

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Image} from 'react-bootstrap'
 import ProfilePic from '../../assets/ProfilePic.png'
 import Cover from '../../assets/ProfileBackground.png'
+import ProfileCard from './../../components/ProfileCard';
 
 function Profile() {
     return(
         <div className="profile-wrapper">
+        <ProfileCard />
             <div className="cover">
                 <Image src = {Cover} fluid />
             </div>
