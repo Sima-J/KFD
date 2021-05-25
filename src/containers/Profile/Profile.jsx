d4afb1dbcb0e920b41c6d140b46a356ccbf5a06a
rebaz36 <rebaz415@gmail.com>
2021-05-25 22:06:50 +0300

    updated profile

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import { Image} from 'react-bootstrap'
-import ProfilePic from '../../assets/ProfilePic.png'
-import Cover from '../../assets/ProfileBackground.png'
 import ProfileCard from '../../components/ProfileCard';
 
-function Profile() {
+export default function Profile() {
     return(
-        <div className="profile-wrapper">
-        <ProfileCard />
-            <div className="cover">
-                <Image src = {Cover} fluid />
-            </div>
-
-            <div className="profile-info">
-                <div className="profile-picture">
-                
-                    <Image 
-                    src={ProfilePic} 
-                    fluid />
-h
-                </div>
-                <div className="info">
-h
-                </div>
-            </div>
+        <div >
+            <ProfileCard />
         </div>
-
-    
-
-        
     )
 }
-
-export default Profile;
