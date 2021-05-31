8354e9146177650255eb1cae64e7e84d34eb9e1d
Rebaz <rebaz415@gmail.com>
2021-06-01 00:18:00 +0300

    Fixed Navigation

--- src/components/DonationInfo/DonationContact.jsx
+++ src/components/DonationInfo/DonationContact.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Col} from 'react-bootstrap'
 
 export default function DonationContact() {
     return (
-        <div>
-          Donation Contact Table
-        </div>
+        
+            <Col >
+              <h1 className= "table">This is just an experiment</h1>
+              <table className ="table">
+                  
+                 <thead>
+                      <tr>
+                          <th>name</th>
+                          <th>height</th>
+                          <th>place</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr>
+                          <td>Monte Falco</td>
+                          <td>1658</td>
+                          <td>Parco Foreste Casentinesi</td>
+                      </tr>
+                      <tr>
+                          <td>Monte Falterona</td>
+                          <td>1654</td>
+                          <td>Parco Foreste Casentinesi</td>
+                      </tr>
+                      </tbody>
+                  </table>
+                  table details 
+            </Col>
+        
     )
 }
