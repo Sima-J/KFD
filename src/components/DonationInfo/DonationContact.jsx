31129a2bd7f35426dc46632f820c69e37e4a00c2
rebaz36 <66506429+rebaz36@users.noreply.github.com>
2021-06-05 04:32:31 +0300

    User card Finalized (#55)
    * Fixed Navigation
    
    * table created - Finalizing left
    
    * User Card Finished

--- src/components/DonationInfo/DonationContact.jsx
+++ src/components/DonationInfo/DonationContact.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import {Col,Image} from 'react-bootstrap'
+import './DonationContact.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import PIcon from '../../assets/ProfileIcon.png'
 
-export default function DonationContact() {
+
+export default function DonationContact() 
+{
     return (
-        <div>
-          Donation Contact Table
-        </div>
-    )
+        
+             <Col>
+
+                <table>
+
+                    <thead>
+
+                        <tr>
+                            <Image className = "PIcon"
+                            src={PIcon}
+                            fluid />
+                            <br/>
+                            User Name Here
+                        </tr>  
+
+                    </thead> 
+                                
+                    <tbody>
+
+                        <td className = "td">
+                            Call the Owner
+                            <br/>
+                            <FontAwesomeIcon className="icon" icon="phone"/>{'  '}
+                            +964 750 113 0495
+                        </td>
+
+                        <tr>
+                            Iraq, Sulaymaniyah, Zerin
+                        </tr>
+
+                        <tr>
+                            Quantity: 36
+                        </tr>
+
+                        <tr>
+                            20/02/2021
+                        </tr>
+
+                        <tr>
+                            Supply Expires in 20 Days
+                        </tr>
+
+                    </tbody>
+
+                </table>          
+
+             </Col>
+        
+             )
 }
+
--- src/components/DonationInfo/DonationSection.jsx
+++ src/components/DonationInfo/DonationSection.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import {
-    Row,
-    Col,
-    Image } from 'react-bootstrap'
+import { Col, Image } from 'react-bootstrap'
 import Donation from '../../assets/donation.png'
 import './DonationSection.scss'
 
-export default function DonationSection() {
+export default function DonationSection() 
+{
     return (
-        <div>        
-            <Row>
+        
                 <Col className = "col">
                     <Image className = "Dimag"
                         src={Donation}
@@ -28,37 +25,6 @@ export default function DonationSection() {
                         used for its benifits or the manfacturing company
                     </h1>
                     
-                
                 </Col>
-
-                <Col >
-                    <h1 className= "table">This is just an experiment</h1>
-                        <table className ="table">
-                        
-                            <thead>
-                            <tr>
-                                <th>name</th>
-                                <th>height</th>
-                                <th>place</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>Monte Falco</td>
-                                <td>1658</td>
-                                <td>Parco Foreste Casentinesi</td>
-                            </tr>
-                            <tr>
-                                <td>Monte Falterona</td>
-                                <td>1654</td>
-                                <td>Parco Foreste Casentinesi</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        table details 
-                </Col>
-            </Row>
-        
-        </div>
-    )
+            )
 }
