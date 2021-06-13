5a1e1aa0aa5de5791d994d50454fe927807ccfe0
jellywiz <muzaffer.dler@gmail.com>
2021-06-13 18:07:04 +0300

    Flter

---
+++ src/components/FliterOption/FliterOption.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FliterOption() {
+    return (
+        <div>
+            Flitert
+        </div>
+    )
+}
--- src/containers/Search/Search.jsx
+++ src/containers/Search/Search.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { SearchHeader } from '../../components/SearchForm'
+import FliterOption from '../../components/FliterOption'
 
 
 export default function Search() {
     return (
         <div>
 <SearchHeader/>
+<FliterOption/>
         </div>
     )
 }
