a1578be210bb8d471ce12267b2d2366cd1c51e35
rebaz36 <66506429+rebaz36@users.noreply.github.com>
2021-06-18 03:59:54 +0300

    Nav bar hidden (#76)
    * NavBar hidden sections set, private routes still in progress

--- src/containers/NotFound/NotFound.jsx
+++ src/containers/NotFound/NotFound.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import logo from '../../assets/logo.svg'
 
 export default function NotFound() {
-    return (
-            <div>
-                Not found Page
-            </div>
-            )}
+  const { t } = useTranslation()
+  return (
+    <div className=" grid p-10 font-bold bg-header h-screen justify-center items-center ">
+      <div className="bg-white text-center gap-2 p-10 bg-opacity-90 shadow-inner grid justify-center  ">
+        <h1 className="text-2xl text-red">{t('notFound.oops')}</h1>
+        <h1 className="">{t('notFound.explain')}</h1>
+        <Link
+          to="/"
+          className="justify-self-center  p-2 text-blue border border-blue rounded-xl mt-3 flex space-x-2  items-center hover:bg-blue hover:text-white"
+        >
+          {t('navbar.home')}
+        </Link>
+        <img
+          src={logo}
+          alt=""
+          className="justify-self-center  p-2  mt-3 flex space-x-2  items-center   max-h-40 "
+        />
+      </div>
+    </div>
+  )
+}
