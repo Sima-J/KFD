0fdcf85c0a963dca496aeac743e79e81682fef5a
Sima-J <76180062+Sima-J@users.noreply.github.com>
2021-06-20 10:44:47 +0300

    Bug Fix (#88)

--- src/components/AddDonationBtn/AddDonationBtn.jsx
+++ src/components/AddDonationBtn/AddDonationBtn.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router-dom'
 
 export default function AddDonationBtn() {
-  const { t } = useTranslation()
 
   const history = useHistory()
   const handleClick = () => {
-    history.push('/addItem')
+    history.push('/adddonation')
   }
   return (
     <button
@@ -16,7 +14,7 @@ export default function AddDonationBtn() {
       onClick={handleClick}
       className="bg-blue  text-md   text-white py-2 text-center  space-x-1 items-center   w-full shadow-md hover:shadow-none rounded-full transition duration-300 ease-in focus:outline-none hover:bg-darkBlue px-3  "
     >
-      <span className=""> {t('home.addItem')} </span>
+      <span className=""> Add Donation </span>
       <FontAwesomeIcon icon="plus" />
     </button>
   )
